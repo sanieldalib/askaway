791df0dcf2137b9d4b6283b49323204059c7c760
Daniel Salib <dsalib@sas.upenn.edu>
2018-04-07 20:26:27 -0400

    init xcode proj

---
+++ AskAway/AskAway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AskAway
+//
+//  Created by Daniel Salib on 4/7/18.
+//  Copyright © 2018 cis195. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
